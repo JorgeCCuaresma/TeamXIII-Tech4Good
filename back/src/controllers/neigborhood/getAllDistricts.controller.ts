b4a8823cdfe723a624f2272df4edc70ef0a2f295
danimorera <danimoreramunoz@gmail.com>
2023-06-20 22:41:37 +0200

    new hoods model running

--- back/src/controllers/neigborhood/getAllDistricts.controller.ts
+++ back/src/controllers/neigborhood/getAllDistricts.controller.ts
@@ -1,20 +1,11 @@
 import { Request, Response } from "express";
 import { Neighborhood } from "../../models/Neighborhood";
-import { INeighborhood } from "../../interfaces/INeighborhood";
 
 export const getAllDistricts = async (req: Request, res: Response) => {
   try {
-    const districts: string[] = await Neighborhood.distinct("district");
+    const districts = await Neighborhood.find();
+    return res.status(200).json(districts);
 
-    const districtData: Record<string, INeighborhood[]> = {};
-    for (const district of districts) {
-      const data: INeighborhood[] = await Neighborhood.find({ district });
-      districtData[district] = data;
-    }
-
-    res.status(200).json({
-      districts: districtData,
-    });
   } catch (error) {
     return res.status(500).json({
       msg: "Server problem when showing stats",
--- back/src/models/Neighborhood.ts
+++ back/src/models/Neighborhood.ts
@@ -2,46 +2,43 @@ import { Schema, model } from "mongoose";
 import { INeighborhood } from "../interfaces/INeighborhood";
 
 const NeighborhoodSchema = new Schema<INeighborhood>({
-    districtCode: {
-        type: Number,
-        required: true,
-    },
-    districtName: {
-        type: String,
-        required: true,
-    },
-    neighborhoodCode: {
-        type: Number,
-        required: true,
-    },
-    neighborhoodName: {
-        type: String,
-        required: true,
-    },
-    incomeAvg: {
-        type: Number,
-        required: true,
-    },
-    incomeScore: {
-        type: Number,
-        required: true,
-    },
-    centerScore: {
-        type: Number,
-        required: true,
-    },
-    digitalScore: {
-        type: Number,
-        required: true,
-    },
-    mobilityScore: {
-        type: Number,
-        required: true,
-    },
-    totalScore: {
-        type: Number,
-        required: true,
-    }
+  districtCode: {
+    type: Number,
+    required: true,
+  },
+  districtName: {
+    type: String,
+    required: true,
+  },
+  neighborhoodCode: {
+    type: Number,
+    required: true,
+  },
+  neighborhoodName: {
+    type: String,
+    required: true,
+  },
+  incomeAvg: {
+    type: Number,
+  },
+  incomeScore: {
+    type: Number,
+  },
+  centerScore: {
+    type: Number,
+  },
+  digitalScore: {
+    type: Number,
+  },
+  mobilityScore: {
+    type: Number,
+  },
+  totalScore: {
+    type: Number,
+  },
 });
 
-export const Neighborhood = model<INeighborhood>("Neighborhood", NeighborhoodSchema);
+export const Neighborhood = model<INeighborhood>(
+  "Neighborhood",
+  NeighborhoodSchema
+);

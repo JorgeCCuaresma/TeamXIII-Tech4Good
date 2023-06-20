8f978cc8bd5618046bc8b6ed6c287cd7f20af16f
danimorera <danimoreramunoz@gmail.com>
2023-06-20 22:02:00 +0200

    updated interface with scores

--- back/src/interfaces/INeighborhood.ts
+++ back/src/interfaces/INeighborhood.ts
@@ -1,6 +1,13 @@
 export interface INeighborhood {
     _id:string;
-    name:string;
-    income:number;
-    district:string;
+    districtId:number;
+    districtName:string;
+    neighborhoodCode:number;
+    neighborhoodName:string;
+    incomeAvg:number;
+    incomeScore:number;
+    centerScore:number;
+    digitalScore:number;
+    mobilityScore:number;
+    totalScore:number;
 }
